a302e6e65ec74ba3d5e8cdd1ea349a697d041ca9
Shirley Esteves <estevesshirley@gmail.com>
2024-11-12 12:28:25 -0300

    Initial Commit

---
+++ cadastro-entregas-api/src/test/java/com/example/cadastro_entregas_api/CadastroEntregasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_entregas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEntregasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
